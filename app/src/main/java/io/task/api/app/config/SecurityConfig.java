214b921857dacbd31d78afbc9bcbcfeaa64f6680
evgeny <evgeny@usachev.fr>
2024-01-22 11:54:47 +0100

    update security config to limit acces to auth users and to handle error if not

--- app/src/main/java/io/task/api/app/config/SecurityConfig.java
+++ app/src/main/java/io/task/api/app/config/SecurityConfig.java
@@ -1,24 +1,37 @@
 package io.task.api.app.config;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import io.task.api.app.service.PersonDetailsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.task.api.app.service.PersonDetailsService;
+import io.task.api.app.utils.AppConstants;
+
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final Logger LOGGER = Logger.getLogger(SecurityConfig.class.getName());
@@ -32,32 +45,55 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.jwtFilter = jwtFilter;
     }
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.cors().and().csrf().disable()
-            .authorizeRequests()
-            //.antMatchers(AppConstants.PUBLIC_URLS).permitAll()
-            //.antMatchers(AppConstants.USER_PATH).hasAnyRole("ADMIN", "USER", "SUPERUSER")
-            .anyRequest().permitAll()
-            //.anyRequest().hasAnyRole("ADMIN")
-            .and()
-            .logout()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
-    // setting authentication
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService).passwordEncoder(getPasswordEncoder());
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers(AppConstants.LOGIN_PATH, "/error").permitAll()
+                .anyRequest().hasAnyRole("ADMIN", "USER", "SUPERUSER")
+                .and()
+                .logout()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.exceptionHandling().accessDeniedHandler(new AccessDeniedHandlerImpl() {
+            @Override
+            public void handle(HttpServletRequest request, HttpServletResponse response,
+                    AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                // Log the access denial
+                LOGGER.warning("Access denied for user " + request.getUserPrincipal().getName() +
+                        " to endpoint " + request.getRequestURI());
+
+                // Customize the response
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.setContentType("application/json");
+
+                // Create a JSON error message
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("error", "Access denied");
+                errorResponse.put("message", "Full authentication is required to access this endpoint");
+
+                // Write the JSON response to the client
+                response.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse));
+
+            }
+
+        }).and();
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -66,5 +102,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
 }
